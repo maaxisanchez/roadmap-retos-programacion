1dc62e338acf482a9805d0ffd4c1f810279b33f3
maaxisanchez <maaxisanchez1997@gmail.com>
2025-09-07 20:02:27 -0300

    maaxisanchez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/maaxisanchez.py
@@ -0,0 +1,35 @@
+# https://python.org
+
+# comentario en una linea
+
+"""
+Esto tambien es
+un comentario
+en varias lineas
+"""
+
+'''
+Esto tambien es 
+otra forma de
+escribir comentarios 
+en python
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convencion
+
+my_int = 1
+my_float = 1.5 
+my_bool = True
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = 'mi otra cadena de texto'
+
+print("Hola python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
